c0f8a52d859ebd5feee4657724513bffe6fe05ca
Sam Plews <the@codeite.com>
2023-02-25 16:23:25 +0000

    abandoned

--- ast/Expression.js
+++ ast/Expression.js
@@ -1,25 +1,39 @@
 const AstItem = require('./AstItem');
 
 module.exports = class Expression extends AstItem {
-  constructor() {
-    super();
+  constructor(assignmentExpressions) {
+    super(...assignmentExpressions);
+  }
+
+  run(scope) {
+    return this.runChildren(scope);
   }
 
   // Expression	::=	AssignmentExpression ( "," AssignmentExpression )*
   static read(ctx) {
     ctx.dlog('readExpression');
     ctx.noIn.push(false);
-    const assignmentExpression = AssignmentExpression.read(ctx);
-    ctx.noIn.pop();
-    return assignmentExpression;
+    return Expression.readCommon(ctx);
   }
 
   static readNoIn(ctx) {
     ctx.dlog('readExpressionNoIn');
     ctx.noIn.push(true);
-    const assignmentExpression = AssignmentExpression.read(ctx);
+    return Expression.readCommon(ctx);
+  }
+
+  static readCommon(ctx) {
+    const assignmentExpressions = [AssignmentExpression.read(ctx)];
+    while (ctx.itr.peek.v === ',') {
+      ctx.itr.read(',');
+      assignmentExpressions.push(AssignmentExpression.read(ctx));
+    }
     ctx.noIn.pop();
-    return assignmentExpression;
+    if (assignmentExpressions.length === 1) {
+      return assignmentExpressions[0];
+    } else {
+      return new Expression(assignmentExpressions);
+    }
   }
 };
 
--- example.js
+++ example.js
@@ -3,19 +3,10 @@ const { createRootScope, unlockScope } = require('./ast/helpers/Scope');
 const Program = require('./ast/Program');
 
 const src = `
-// An object can be passed as the first argument to call or apply and this will be bound to it.
-var obj = {a: 'Custom'};
-
-// This property is set on the global object
-var a = 'Global';
-
-function whatsThis(i, j) {
-  return this.a + (i + j);  // The value of this is dependent on how the function is called
+function f() {
+  return 1,2;
 }
-
-var x = whatsThis(1, 2);      // 'Global3'
-var y = whatsThis.call(obj, 1, 2);  // 'Custom'
-var z = whatsThis.apply(obj); // 'Custom'
+var x = f()
 `;
 
 // console.log('[...tokenize(src)]:', [...tokenize(src)]);
